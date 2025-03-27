87d7365ca11129a15142a471364f7986081508a2
newusually <49580033+newusually@users.noreply.github.com>
2025-03-27 12:34:03 +0800

    Add files via upload

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -6,7 +6,8 @@ import (
 	"os/exec"
 	"strings"
 	"time"
-	//"regexp"
+
+	"github.com/markcheno/go-talib"
 )
 
 func Run1() {
@@ -68,9 +69,27 @@ func Run(minute string) {
 	symbollist := mvc.Getsymbollist()
 	for _, symbol := range symbollist {
 
-		x, c, c2, c1, _, _, _, _, _ := mvc.GetKline(strings.TrimSuffix(symbol, "-USDT-SWAP"), "15m")
+		x, c, c2, c1, o, _, _, _, _ := mvc.GetKline(strings.TrimSuffix(symbol, "-USDT-SWAP"), minute)
+		if x < 10 {
+			continue
+		}
+		c3 := c[x-3] / o[x-3]
+		c4 := c[x-4] / o[x-4]
+		c5 := c[x-5] / o[x-5]
+		c6 := c[x-6] / o[x-6]
+		c7 := c[x-7] / o[x-7]
+		diff, dea, _ := talib.Macd(c, 12, 26, 60)
+		macd1 := 2 * (diff[x-1] - dea[x-1])
+		macd2 := 2 * (diff[x-2] - dea[x-2])
+		macd3 := 2 * (diff[x-3] - dea[x-3])
 
-		isbuy := c2 < 0.95 && c2 > 0.9 && c1 > 0.9 && c1 < 0.98 && c[x-1] > 0.0001
+		buy1 := c2 < 0.95 && c2 > 0.9 && c1 > 0.9 && c1 < 0.98
+		buy2 := c3 > 0.9 && c3 < 0.99 && c2 > 0.9 && c2 < 0.99 && c1 > 0.9 && c1 < 0.99
+		buy3 := macd1 < 0 && macd2 < 0 && macd3 < 0 && c1 < 0.99 && c1 > 0.95
+		buy4 := macd1 < macd2 && macd2 < macd3 && c1 < 0.99 && c1 > 0.95
+		buy5 := c1 > 0.9 && c1 < 1 && c2 > 0.9 && c2 < 1 && c3 > 0.9 && c3 < 1 && c4 > 0.9 && c4 < 1 && c5 > 0.9 && c5 < 1 && c6 > 0.9 && c6 < 1 && c7 > 0.9 && c7 < 1
+
+		isbuy := (buy1 || buy2 || buy3 || buy4 || buy5) && c[x-1] > 0.0001
 		if isbuy {
 
 			fmt.Println(time.Now().Format("2006-1-2 15:04:02") + "-------------------------------买入--------------------------------->>>")
